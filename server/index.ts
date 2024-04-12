cdf468e1d9839e9a4b5fd1a7f65809a084bfabf0
dayvidwhy <dayvidwhy@gmail.com>
2024-04-12 19:36:18 +1000

    Use context to check if logged in.

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,12 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import db from "@db/database";
 import { hash, compare } from "@server/hash";
+import { GraphQLError } from "graphql";
 
 const typeDefs = `#graphql
     type Query {
         getUser(id: ID!): User,
         getAllUsers: [User],
-        login(email: String!, password: String!): User
     }
 
     type Mutation {
@@ -35,7 +35,15 @@ const typeDefs = `#graphql
 
 const resolvers = {
     Mutation: {
-        deleteUser: async (_, { id }, { users }) => {
+        deleteUser: async (_, { id }, { users, isLoggedIn }) => {
+            if (!isLoggedIn) {
+                throw new GraphQLError("User is not authenticated", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
             const user = await users.findByPk(id);
             await user.destroy();
             return user;
@@ -49,7 +57,15 @@ const resolvers = {
             });
             return user;
         },
-        updateUser: async (_, { id, name, email }, { users }) => {
+        updateUser: async (_, { id, name, email }, { users, isLoggedIn }) => {
+            if (!isLoggedIn) {
+                throw new GraphQLError("User is not authenticated", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
             const user = await users.findByPk(id);
             user.name = name;
             user.email = email;
@@ -58,29 +74,27 @@ const resolvers = {
         },
     },
     Query: {
-        getUser: async (_, { id }, { users }) => {
+        getUser: async (_, { id }, { users, isLoggedIn }) => {
+            if (!isLoggedIn) {
+                throw new GraphQLError("User is not authenticated", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
             return await users.findByPk(id);
         },
-        getAllUsers: async (_, __, { users }) => {
+        getAllUsers: async (_, __, { users, isLoggedIn }) => {
+            if (!isLoggedIn) {
+                throw new GraphQLError("User is not authenticated", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
             return await users.findAll();
-        },
-        login: async (_, { email, password }, { users, passwords }) => {
-            const user = await users.findOne({ where: { email } });
-            if (!user) {
-                throw new Error("Authentication failed.");
-            }
-
-            const passwordHash = await passwords.findOne({ where: { id: user.id } });
-
-            if (!passwordHash) {
-                throw new Error("Authentication failed.");
-            }
-
-            if (!(await compare(password, passwordHash.password))) {
-                throw new Error("Authentication failed.");
-            }
-
-            return user;
         }
     }
 };
@@ -92,10 +106,62 @@ const server = new ApolloServer({
 
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
-    context: async () => {
+    context: async ({ req }) => {
+        let isLoggedIn = false;
+        const auth = req.headers.authorization;
+        if (auth) {
+            const authFormat = new RegExp(/^Basic [A-Za-z0-9+/]+={0,2}$/);
+            if (authFormat.test(auth) === false) {
+                throw new GraphQLError("Auth failure", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
+
+            const base64Credentials = auth.split(" ")[1];
+            const [username, password] = Buffer.from(base64Credentials, "base64").toString().split(":");
+            console.log(username, password);
+            const user = await db.Users.findOne({ where: { email: username } });
+            if (!user) {
+                throw new GraphQLError("Auth failure", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
+
+            const passwordHash = await db.Passwords.findOne({ where: { id: (user as any).id } });
+
+            if (!passwordHash) {
+                throw new GraphQLError("Auth failure", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
+
+            const passwordHashValue = passwordHash.get("password") as string;
+
+            if (await compare(password, passwordHashValue)) {
+                isLoggedIn = true;
+            } else {
+                throw new GraphQLError("Auth failure", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
+        }
+
         return {
             users: db.Users,
-            passwords: db.Passwords
+            passwords: db.Passwords,
+            isLoggedIn
         };
     }
 });
